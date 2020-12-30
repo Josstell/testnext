61ffcc85549e8a7d4a43b06ed7bb45360d516d83
José Juan Téllez Guzmán <jjtellezg@gmail.com>
2020-12-29 21:11:29 -0600

    Contenido generado por el servidor, server side rendering.

--- pages/posts/index.js
+++ pages/posts/index.js
@@ -6,27 +6,29 @@ import Link from 'next/link'
 
 
 
-export default function Posts() {
-  const [posts, setPosts] = useState([]);
+export default function Posts({posts}) {
 
-  useEffect(() => {
-    const fetchPosts = async () => {
-      const res = await fetch("https://jsonplaceholder.typicode.com/posts");
-      const newpost = await res.json();
-      setPosts(newpost);
-    };
-    fetchPosts();
-  }, []);
+  //Codigo utilizado para Client side rendering
+  // const [posts, setPosts] = useState([]);
+
+  // useEffect(() => {
+  //   const fetchPosts = async () => {
+  //     const res = await fetch("https://jsonplaceholder.typicode.com/posts");
+  //     const newpost = await res.json();
+  //     setPosts(newpost);
+  //   };
+  //   fetchPosts();
+  // }, []);
 
   return (
     <Layout>
       <Title>Posts Page</Title>
-      <div>
+      <div className='grid'>
         {
           posts.map(post => {
             return (
               <Link href={`/posts/${post.id}`} key={post.id}>
-                <a>
+                <a className='card'>
                   <h3>{post.title}</h3>
                   <p>{post.body}</p>
                 </a>
@@ -35,6 +37,53 @@ export default function Posts() {
           })
         }
       </div>
+      <style jsx>
+        {`
+          .grid {
+            display: flex;
+            flex-wrap: wrap;
+            max-width: 800px;
+            margin-top: 2rem;
+          }
+          .card {
+            margin: 1rem;
+            flex-basis: 45%;
+            padding: 1.5rem;
+            color: black;
+            text-decoration: none;
+            border: 2px solid #eaeaea;
+            border-radius: 10px;
+            transition: color 0.15s ease, border-color 0.15s ease;
+          }
+          .card:hover,
+          .card:focus,
+          .card:active {
+            color: #0070f3;
+            border-color: #0070f3;
+          }
+          .card h3 {
+            margin: 0 0 1rem 0;
+            font-size: 1.5rem;
+          }
+          .card p {
+            margin: 0;
+            font-size: 1.25rem;
+            line-height: 1.5;
+          }
+        `}
+      </style>
     </Layout>
   );
 }
+
+export async function getServerSideProps(){
+     const res = await fetch("https://jsonplaceholder.typicode.com/posts");
+     const posts = await res.json();
+
+     return {
+       props:{
+         posts
+       }
+     }
+
+    }
